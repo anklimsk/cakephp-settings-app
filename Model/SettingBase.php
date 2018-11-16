62c8ab15ca708010cd3de9cd1806dfdbd9c3018d
Andrey Klimov <anklimsk@gmail.com>
2018-11-16 15:58:59 +0300

    Fix property `useDbConfig` in model `SettingBase`

--- Model/SettingBase.php
+++ Model/SettingBase.php
@@ -32,17 +32,6 @@ class SettingBase extends CakeSettingsAppAppModel {
 	public $name = 'Setting';
 
 /**
- * The name of the DataSource connection that this Model uses
- *
- * The value must be an attribute name that you defined in `app/Config/database.php`
- * or created using `ConnectionManager::create()`.
- *
- * @var string
- * @link http://book.cakephp.org/2.0/en/models/model-attributes.html#usedbconfig
- */
-	public $useDbConfig = false;
-
-/**
  * Custom database table name, or null/false if no table association is desired.
  *
  * @var string
